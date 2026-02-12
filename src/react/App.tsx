fd7ff110f7744936dbefd5475e40335994b7e33b
Casey <103451081+dev-syn@users.noreply.github.com>
2026-02-12 08:10:29 -0500

    Add base and text styles (Start)

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,16 +1,21 @@
 import React from '@rbxts/react';
-import { CloseBtn } from '@rbxts/syn-defaults/components/CloseBtn';
+import { useBaseStyles, useTextStyles, CloseBtn } from '@rbxts/syn-defaults';
 
 export function App() {
+
+	const base = useBaseStyles();
+	const text = useTextStyles();
+
 	return (
 	<frame
+		{ ...base.surface }
 		AnchorPoint={new Vector2(0.5,0.5)}
 		Size={UDim2.fromOffset(450,700)}
 		Position={UDim2.fromScale(0.5,0.5)}
 	>
 		<CloseBtn
-		_initClosed={false}
-		onClose={() => print("App close.")}
+			_initClosed={false}
+			onClose={() => print("App close.")}
 		></CloseBtn>
 	</frame>
 	);
