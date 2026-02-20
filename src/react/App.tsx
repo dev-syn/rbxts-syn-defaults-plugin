fde7de4d4400429b803725e81621e2cbcf7b1300
Casey <103451081+dev-syn@users.noreply.github.com>
2026-02-20 10:48:28 -0500

    Add header props and a window size for the new AppConfig

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,26 +1,44 @@
-import React from '@rbxts/react';
-import { useBaseStyles, useTextStyles, CloseBtn, useTheme } from '@rbxts/syn-defaults';
+import React, { useMemo } from '@rbxts/react';
+import { useBaseStyles, useTextStyles, useTheme, SynText, centerX } from '@rbxts/syn-defaults';
+import { APP_MIN_WIDTH, APP_MIN_HEIGHT } from '../constants';
 
-export function App() {
+interface AppConfig {
+	windowSize?: UDim2;
+}
 
+export function App(config: AppConfig) {
+	const { windowSize=UDim2.fromOffset(APP_MIN_WIDTH,APP_MIN_HEIGHT) } = config;
 	const theme = useTheme();
 	
 	const base = useBaseStyles();
 	const text = useTextStyles();
 
+	const appHeaderProps = useMemo<React.InstanceProps<TextLabel>>(() => ({
+		AnchorPoint: new Vector2(0.5,0),
+		Position: new UDim2(0.5,0,0,25),
+
+		TextColor3: theme.colors.text.primary
+	}),[ theme ]);
+
 	return (
-	<frame
-		BackgroundColor3={theme.colors.background}
-		AnchorPoint={new Vector2(0.5,0.5)}
-		Size={UDim2.fromOffset(500,350)}
-		Position={UDim2.fromScale(0.5,0.5)}
-	>
-		<uistroke
-			{...base.border}
-		/>	
-		<CloseBtn
-			onClose={() => print("App close.")}
-		/>
-	</frame>
+		<frame
+			BackgroundColor3={theme.colors.background}
+			AnchorPoint={new Vector2(0.5,0.5)}
+			Size={windowSize}
+			Position={UDim2.fromScale(0.5,0.5)}
+		>
+			<SynText<'tl'>
+				instType='tl'
+				sizeVariant='h2'
+				content='Syn Defaults'
+				native={appHeaderProps}
+			>
+			</SynText>
+			<frame {...base.divider } Position={new UDim2(0.5,0,0,50)} ></frame>
+			<frame
+				BackgroundTransparency={0.5} // {...base.container}
+				Size={new UDim2(1,0,)}
+			></frame>
+		</frame>
 	);
 }
