6f8724a161a5fd4b67e2047e73593b1783b52531
Casey <103451081+dev-syn@users.noreply.github.com>
2026-03-01 19:03:56 -0500

    Add FieldSize UDim2control and control size

--- src/react/stories/syn_defaults_stories/SynField.story.tsx
+++ src/react/stories/syn_defaults_stories/SynField.story.tsx
@@ -1,13 +1,51 @@
-import React from '@rbxts/react';
+import React, { useMemo } from '@rbxts/react';
 import ReactRoblox from '@rbxts/react-roblox';
 import { StoryWrapper, SynField, SynFieldVariant } from '@rbxts/syn-defaults/index';
-import { Choose, CreateReactStory } from '@rbxts/ui-labs';
+import { Choose, ControlGroup, CreateReactStory, Number } from '@rbxts/ui-labs';
 
 const variants = ["Horizontal","Vertical"];
 type VariantType =  keyof typeof SynFieldVariant;
 
 const controls = {
-	Variant: Choose(variants,1,true)
+	Variant: Choose(variants,1,true),
+
+	FieldSizeX: ControlGroup({
+		Scale: Number(
+			0, // default
+			-5, // min
+			5, // max
+			0.05, // step
+			true, // dragger
+			0.7 // sens
+		),
+		Offset: Number(
+			200, // default
+			-1000000, // min
+			1000000, // max
+			1, // step
+			true, // dragger
+			0.7 // sens
+		)
+	}),
+	FieldSizeY: ControlGroup({
+		Scale: Number(
+			0, // default
+			-5, // min
+			5, // max
+			0.05, // step
+			true, // dragger
+			0.7 // sens
+		),
+		Offset: Number(
+			75, // default
+			-1000000, // min
+			1000000, // max
+			1, // step
+			true, // dragger
+			0.7 // sens
+		)
+	}),
+
 };
 
 const story = CreateReactStory({
@@ -17,11 +55,16 @@ const story = CreateReactStory({
 },(props) => {
 	// Map the control string directly to the Enum value
 	const selectedVariant = SynFieldVariant[props.controls.Variant as VariantType];
+
+	const fieldSizeX = props.controls.FieldSizeX;
+	const fieldSizeY = props.controls.FieldSizeY;
+
 	return (
 		<StoryWrapper>
 			<SynField
 				variant={selectedVariant}
-				
+				size={new UDim2(fieldSizeX.Scale,fieldSizeX.Offset,fieldSizeY.Scale,fieldSizeY.Offset)}
+				content=""
 			></SynField>
 		</StoryWrapper>
 	)
