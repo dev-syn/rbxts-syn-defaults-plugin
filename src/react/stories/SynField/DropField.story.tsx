0e379ac5c41dcaa39a59d3376b552ffc8ce54c9c
Casey <103451081+dev-syn@users.noreply.github.com>
2026-03-10 01:51:29 -0400

    init

---
+++ src/react/stories/SynField/DropField.story.tsx
@@ -0,0 +1,82 @@
+import { deunify } from '@rbxts/flamework-meta-utils';
+import React from '@rbxts/react';
+import ReactRoblox from '@rbxts/react-roblox';
+import { StoryWrapper, SynField, SynFieldLayoutPosition, SynFieldVariant } from '@rbxts/syn-defaults/index';
+import { Choose, ControlGroup, CreateReactStory, Number, String } from '@rbxts/ui-labs';
+
+type LayoutPositionVariant = keyof typeof SynFieldLayoutPosition;
+const layoutPositionVariants = deunify<LayoutPositionVariant>();
+
+const controls = {
+	LayoutPosition: Choose(layoutPositionVariants,1,false),
+
+	Content: ControlGroup({
+		LabelContent: String("Label"),
+	},1),
+
+	FieldSizeX: ControlGroup({
+		Scale: Number(
+			0, // default
+			-5, // min
+			5, // max
+			0.05, // step
+			true, // dragger
+			0.7 // sens
+		),
+		Offset: Number(
+			200, // default
+			-1000000, // min
+			1000000, // max
+			1, // step
+			true, // dragger
+			0.7 // sens
+		)
+	},2),
+	FieldSizeY: ControlGroup({
+		Scale: Number(
+			0, // default
+			-5, // min
+			5, // max
+			0.05, // step
+			true, // dragger
+			0.7 // sens
+		),
+		Offset: Number(
+			75, // default
+			-1000000, // min
+			1000000, // max
+			1, // step
+			true, // dragger
+			0.7 // sens
+		)
+	},3)
+};
+
+const story = CreateReactStory({
+	react: React,
+	reactRoblox: ReactRoblox,
+	controls: controls
+},(props) => {
+	const selectedLayoutPosVariant = SynFieldLayoutPosition[props.controls.LayoutPosition as LayoutPositionVariant];
+	const fieldSizeX = props.controls.FieldSizeX;
+	const fieldSizeY = props.controls.FieldSizeY;
+	const content = props.controls.Content;
+
+	return (
+		<StoryWrapper>
+			<SynField
+				variant={SynFieldVariant.Dropdown}
+				dropdownProps={{
+					menuId: "ui-labs-FieldDropdown",
+					choices: ["Option_A","Option_B","Option_C"],
+					_default: 0,
+					menuOptions: {}
+				}}
+				layoutPos={selectedLayoutPosVariant}
+				size={new UDim2(fieldSizeX.Scale,fieldSizeX.Offset,fieldSizeY.Scale,fieldSizeY.Offset)}
+				labelContent={content.LabelContent}
+			></SynField>
+		</StoryWrapper>
+	)
+});
+export = story;
